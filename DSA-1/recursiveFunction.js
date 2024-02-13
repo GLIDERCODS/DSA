77bee41da59a8bd06b247ee23e4bd6147427b264
Shibili <shibilimp063@gmail.com>
2024-02-13 14:01:30 +0530

    third commit

--- DSA-1/recursiveFunction.js
+++ DSA-1/recursiveFunction.js
@@ -1,23 +1,23 @@
                   /*  FIBONACCI SEQUENCE */
 
-// function fibonacci(num){
-//     if(num < 2){
-//         return num
-//     }
+function fibonacci(num){
+    if(num < 2){
+        return num
+    }
 
-//     return fibonacci(num-1) + fibonacci(num-2)
-// }
+    return fibonacci(num-1) + fibonacci(num-2)
+}
 
-//  console.log(fibonacci(6));  
+ console.log(fibonacci(6));  
 
                   /* FACTORIAL */
 
-//   function factorial(num){
-//     if(num === 1){
-//         return 1
-//     }
+  function factorial(num){
+    if(num === 1){
+        return 1
+    }
      
-//     return num*factorial(num-1)
+    return num*factorial(num-1)
     
-// }
-// console.log(factorial(5))
+}
+console.log(factorial(5))
---
+++ DSA-2/Sorting.js
@@ -0,0 +1,39 @@
+/* ===== Bubble Sort ===== */
+
+// function bubbleSort(arr){
+//     let swaped
+//     do {
+//         swaped = false
+//         for(let i=0 ; i<arr.length-1 ;i++){
+//             if(arr[i]>arr[i+1]){
+//                 let temp = arr[i+1]
+//                 arr[i+1] = arr[i]
+//                 arr[i] = temp 
+//                 swaped = true
+//             }
+//         }
+        
+//     } while (swaped);
+// }
+
+// const array = [6,-1,7,-4,3,8,5]
+// bubbleSort(array)
+// console.log(array);
+
+/*  =====  Insertion Sort  ===== */
+
+function insertionsort(arr){
+    for(let i = 1 ; i < arr.length ; i++){
+        let temp = arr[i]
+        let j = i-1
+        while( j >= 0 && arr[j] > temp ){
+            arr[j+1] = arr[j]
+            j = j-1
+        }
+        arr[j+1] = temp
+    }
+}
+
+const array = [4,6,-3,5,2,1]
+insertionsort(array)
+console.log(array);
