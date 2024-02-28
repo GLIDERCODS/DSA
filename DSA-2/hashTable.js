d40c14ec5fa4bbe86fffd2fff61c1e2a1f5168ac
Shibili <shibilimp063@gmail.com>
2024-02-29 00:00:47 +0530

    mid remve frome stack

--- DSA-2/hashTable.js
+++ DSA-2/hashTable.js
@@ -3,40 +3,63 @@ class HashTable{
         this.table = new Array(size)
         this.size = size
     }
-
     hash(key){
         let total = 0
-        for(let i = 0 ; i < key.length ; i++){
+        for(let i=0;i<key.length;i++){
             total += key.charCodeAt(i)
         }
-        return total % this.size
+        return total%this.size
     }
-
     set(key,value){
-        const index = this.hash(key)
-        this.table[index] = value
+        let index = this.hash(key)
+        let bucket = this.table[index]
+        if(!bucket){
+            this.table[index]=[[key,value]]
+        }else{
+            let sameKey = bucket.find(item=>item[0]===key)
+            if(sameKey){
+                sameKey[1]=value
+            }else{
+                bucket.push([key,value])
+            }
+        }
     }
-
     get(key){
-        const index = this.hash(key)
-        return this.table[index]
+        let index = this.hash(key)
+        let bucket = this.table[index]
+        if(bucket){
+            let sameKey=bucket.find(item=>item[0]===key)
+            if(sameKey){
+                return sameKey[1]
+            }
+        }
+        return undefined
     }
-
     remove(key){
-        const index = this.hash(key)
-        this.table[index] = undefined
+        let index = this.hash(key)
+        let bucket = this.table[index]
+        if(bucket){
+            let sameKey = bucket.find(item=>item[0]===key)
+            if(sameKey){
+                bucket.splice(bucket.indexOf(sameKey),1)
+            }
+        }
     }
-
     display(){
-        for(let i = 0 ; i<this.table.length ; i++){
+        for(let i=0;i<this.table.length;i++){
             if(this.table[i]){
-                console.log(this.table[i])
+                console.log(this.table[i]);
             }
         }
     }
 }
 
-const hashTable = new HashTable()
-hashTable.set("name","shibili")
-hashTable.set("age",45)
-hashTable.display()
+let table = new HashTable(30)
+table.set("name","shibili")
+table.set("age",20)
+table.set("place","tirur")
+
+console.log(table.get("name"));
+table.display()
+table.set("mean","hello")
+table.display()
