84523a19e996576c2eff1fc52581ca22c52393f6
Shibili <shibilimp063@gmail.com>
2024-03-05 12:16:46 +0530

    tree2

--- DSA-3/implimentationOfBinarySearchTree.js
+++ DSA-3/implimentationOfBinarySearchTree.js
@@ -15,7 +15,36 @@ class BinarySearchTree{
     isEmpty(){
         return this.root === null
     }
+
+    insert(value){
+        const node = new Node(value)
+        if(this.isEmpty()){
+             this.root = node
+        }else{
+            this.insertNode(this.root,node)
+        }
+    }
+
+    insertNode(root,node){
+        if(node.value <  root.value){
+            if(root.left === null){
+                root.left = node
+            }else{
+                this.insertNode(root.left,node)
+            }
+        }else{
+            if(root.right === null){
+                root.right = node
+            }else{
+                this.insertNode(root.right,node)
+            }
+        }
+    }
 }
 
 const binarySearchTree = new BinarySearchTree()
 console.log(binarySearchTree.isEmpty())
+
+binarySearchTree.insert(10)
+binarySearchTree.insert(5)
+binarySearchTree.insert(15)
